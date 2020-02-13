724ed2051daa8aff184e66c748a4f37366bc2935
unknown <lastsaintcrucified@gmail.com>
2020-02-13 07:42:36 +0600

    Updates

---
+++ precache-manifest.c0c08aca635f9a56670c281e95810538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c8c31f7d4c93a8a651b3331ac88c45",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "3bcb7d3f41e79c997ac2",
+    "url": "/react-monster/static/css/main.10fe69d2.chunk.css"
+  },
+  {
+    "revision": "53a85757fff4e61cd481",
+    "url": "/react-monster/static/js/2.55d3d37d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monster/static/js/2.55d3d37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcb7d3f41e79c997ac2",
+    "url": "/react-monster/static/js/main.584e1a00.chunk.js"
+  },
+  {
+    "revision": "040dd5b375005c4ce1ec",
+    "url": "/react-monster/static/js/runtime-main.1a4fd90a.js"
+  }
+]);
